1960f59f89a30fa20690baa3610addca6113bbbd
Martin Brochhaus <mbrochh@gmail.com>
2012-12-20 22:43:33 +0800

    Initialized Django project / started splitting up the settings file.

---
+++ matchmaker/matchmaker/settings/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+from matchmaker.settings.base_settings import *
+from matchmaker.settings.django_settings import *
+from matchmaker.settings.local.local_settings import *
---
+++ matchmaker/matchmaker/settings/base_settings.py
@@ -0,0 +1,9 @@
+"""
+Settings that might be useful in our other settings-files.
+
+"""
+import os
+
+# The folder where ./manage.py resides.
+PROJECT_ROOT = os.path.realpath(
+    os.path.join(os.path.dirname(__file__), '../..'))
