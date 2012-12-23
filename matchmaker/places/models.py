42cf398d0d7d8d27a7a3969a76a75951db111f61
Martin Brochhaus <mbrochh@gmail.com>
2012-12-23 20:34:05 +0800

    Added docstrings to places models.

--- matchmaker/places/models.py
+++ matchmaker/places/models.py
@@ -4,6 +4,14 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Place(models.Model):
+    """
+    Any place on earth.
+
+    :name: The name of the place.
+    :point: Lat/Lng of the place.
+    :type: Optional type of the place.
+
+    """
     name = models.CharField(
         max_length=256,
         verbose_name=_('Name'),
@@ -27,6 +35,12 @@ class Place(models.Model):
 
 
 class PlaceType(models.Model):
+    """
+    A masterdata table holding types of places.
+
+    :name: The name of the type.
+
+    """
     name = models.CharField(
         max_length=256,
         verbose_name=_('Name'),
