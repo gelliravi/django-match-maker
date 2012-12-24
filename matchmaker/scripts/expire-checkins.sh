3d2110be0099c4f7c8c3269a8718e4fcec99bd04
Martin Brochhaus <mbrochh@gmail.com>
2012-12-25 00:00:16 +0800

    Fixed typo in expire-checkins.sh

--- matchmaker/scripts/expire-checkins.sh
+++ matchmaker/scripts/expire-checkins.sh
@@ -2,4 +2,4 @@
 source $HOME/bin/script-settings-matchmaker.sh
 source $HOME/Envs/$VENV_NAME/bin/activate
 
-$HOME/webapps/$DJANGO_APP_NAME/$PROJECT_NAME/manage.py expire_checkins
+$HOME/webapps/$DJANGO_APP_NAME/$PROJECTNAME/manage.py expire_checkins
