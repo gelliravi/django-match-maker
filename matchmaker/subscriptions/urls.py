bc17d26d771c13cdfcf43a99e6195d1854407a1c
Martin Brochhaus <mbrochh@gmail.com>
2012-12-26 17:36:16 +0800

    Added subscriptions app skeleton

---
+++ matchmaker/subscriptions/urls.py
@@ -0,0 +1,12 @@
+"""URLs for the ``subscriptions`` app."""
+from django.conf.urls.defaults import patterns, url
+
+# from subscriptions.views import SubscriptionCreateView
+
+
+urlpatterns = patterns(
+    '',
+    #url(r'^create/(?P<place_pk>\d+)/$',
+    #    SubscriptionCreateView.as_view(),
+    #    name='checkins_create',),
+)
