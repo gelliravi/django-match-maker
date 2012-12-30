fcef1bb595c9f1e72453c4e6ca586a194caeddac
Martin Brochhaus <mbrochh@gmail.com>
2012-12-30 16:37:15 +0800

    Added tests for pipeline and user_pofile view

---
+++ matchmaker/matchmaker/tests/pipeline_tests.py
@@ -0,0 +1,47 @@
+"""Tests for the ``pipeline.user`` functions."""
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from django_libs.tests.factories import UserFactory
+
+from matchmaker.pipeline.user import create_user, get_username
+
+
+class CreateUserTestCase(TestCase):
+    """Tests for the ``create_user`` function."""
+    def test_create_user(self):
+        user = UserFactory()
+        resp = create_user(backend=None, details=None, response=None, uid=None,
+                           username=None, user=user)
+        self.assertEqual(resp, {'user': user}, msg=(
+            'Should respond the user instance. Response was {0}'.format(resp)))
+        resp = create_user(backend=None, details=None, response=None, uid=None,
+                           username=None)
+        self.assertIsNone(resp, msg=(
+            'Should respond "None". Response was {0}'.format(resp)))
+        resp = create_user(backend=None, details={'email': 'info@example.com'},
+                           response=None, uid=None, username=user.username)
+        self.assertEqual(resp, {'user': user, 'is_new': False}, msg=(
+            'Should respond the user instance plus "not new"-notification.'
+            'Response was {0}'.format(resp)))
+        resp = create_user(backend=None, details={'email': 'info@example.com'},
+                           response=None, uid=None, username='Foo')
+        self.assertEqual(
+            resp,
+            {'user': User.objects.get(username='Foo'), 'is_new': True},
+            msg=('Should respond the user instance plus "new"-notification.'
+            'Response was {0}'.format(resp)))
+
+    """Tests for the ``get_username`` function."""
+    def test_get_username(self):
+        user = UserFactory()
+        resp = get_username(details={'email': user.email}, user=user)
+        self.assertEqual(resp, {'username': user.username}, msg=(
+            'Should respond the username. Response was {0}'.format(resp)))
+        resp = get_username(details={'email': user.email})
+        self.assertEqual(resp, {'username': user.username}, msg=(
+            'Should respond the username. Response was {0}'.format(resp)))
+        resp = get_username(details={'email': 'info@example.com'})
+        self.assertTrue(resp['username'], msg=(
+            'Should return a generated username. Response was'
+            ' {0}'.format(resp['username'])))
--- matchmaker/user_profile/tests/integration_tests/views_tests.py
+++ matchmaker/user_profile/tests/integration_tests/views_tests.py
@@ -4,15 +4,41 @@ from django.test import TestCase
 from django_libs.tests.factories import UserFactory
 from django_libs.tests.mixins import ViewTestMixin
 
+from user_profile.models import UserProfile
+from user_profile.tests.factories import UserProfileFactory
+
 
 class UserProfileUpdateViewTestCase(ViewTestMixin, TestCase):
     """Tests for the ``UserProfileUpdateView`` view class."""
+    def setUp(self):
+        super(UserProfileUpdateViewTestCase, self).setUp()
+        self.profile = UserProfileFactory()
+        self.post_data = {
+            'timezone': 'Asia/Singapore',
+        }
+
     def get_view_name(self):
         return 'user_profile_update'
 
-    def setUp(self):
-        super(UserProfileUpdateViewTestCase, self).setUp()
-        self.user = UserFactory()
-
     def test_login_reqired(self):
+        """Should refirect to login if anonymous."""
         self.should_redirect_to_login_when_anonymous()
+
+    def test_callable(self):
+        """Should be callable if user is authenticated."""
+        self.should_be_callable_when_authenticated(self.profile.user)
+
+    def test_creates_profile(self):
+        """Should create a profile if a user doesn't have one."""
+        user_without_profile = UserFactory()
+        self.login(user_without_profile)
+        self.client.get(self.get_url())
+        profile = user_without_profile.get_profile()
+        self.assertTrue(profile.pk)
+
+    def test_save(self):
+        """Should save the profile on POST request."""
+        self.login(self.profile.user)
+        self.client.post(self.get_url(), data=self.post_data)
+        profile = UserProfile.objects.get(pk=self.profile.pk)
+        self.assertEqual(profile.timezone, 'Asia/Singapore')
