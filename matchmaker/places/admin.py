aa86db52c130b05e68547c005b43a7c7f88e1aca
Martin Brochhaus <mbrochh@gmail.com>
2013-01-08 21:51:15 +0800

    Added GoogleMapsAdmin

--- matchmaker/places/admin.py
+++ matchmaker/places/admin.py
@@ -4,5 +4,11 @@ from django.contrib.gis import admin
 from places.models import Place, PlaceType
 
 
-admin.site.register(Place, admin.GeoModelAdmin)
+class GoogleAdmin(admin.OSMGeoAdmin):
+    map_template = 'gis/admin/google.html'
+    extra_js = [
+        'https://maps.google.com/maps?file=api&v=3&sensor=false&callback=initialize']
+
+
+admin.site.register(Place, GoogleAdmin)
 admin.site.register(PlaceType)
