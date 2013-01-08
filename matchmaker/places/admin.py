322a75bbe9fd961aa5287850c99f4198e68ffb1e
Martin Brochhaus <mbrochh@gmail.com>
2013-01-08 22:03:46 +0800

    Added API_KEY setting.

--- matchmaker/places/admin.py
+++ matchmaker/places/admin.py
@@ -1,14 +1,18 @@
 """Admin classes for the ``places`` app."""
+from django.conf import settings
 from django.contrib.gis import admin
 
 from places.models import Place, PlaceType
 
 
-class GoogleAdmin(admin.OSMGeoAdmin):
+class GoogleMapsAdmin(admin.OSMGeoAdmin):
     map_template = 'gis/admin/google.html'
     extra_js = [
-        'https://maps.google.com/maps?file=api&v=3&sensor=false&callback=initialize']
+        'https://maps.google.com/'
+        'maps?file=api&v=3&sensor=false&callback=initialize&key={0}'.format(
+            settings.GOOGLE_MAPS_API_KEY),
+    ]
 
 
-admin.site.register(Place, GoogleAdmin)
+admin.site.register(Place, GoogleMapsAdmin)
 admin.site.register(PlaceType)
